8ab41375cb68f92251cd30232d1af4a4f559b830
hilton92 <benjamin.hilton92@gmail.com>
2018-11-23 21:10:24 -0700

    fixed revert errors

---
+++ OSGLarynx.h
@@ -0,0 +1,11 @@
+
+// Author: Benjamin Hilton
+// Date: November 2018
+
+#ifndef OSGLARYNX_H_
+#define OSGLARYNX_H_
+#include <osg/Node>
+
+osg::ref_ptr<osg::Node> create_cartilage(unsigned int modelNum);
+
+#endif
