1ea1d1af41a5b04981502c1293a2fc88525ef63c
hilton92 <benjamin.hilton92@gmail.com>
2018-11-30 23:51:41 -0700

    movement in progress

--- ThyroidUpdateCallback.h
+++ ThyroidUpdateCallback.h
@@ -15,18 +15,30 @@ public:
     {
         if (progRunning)
         {
-            double newDispX = 1.0;
-            double newDispY = 2.0;
-            double newDispZ = 3.0;
-            osg::Vec3d trans_position(newDispX, newDispY, newDispZ);
+            if(mUp)
+                mCount++;
+            else
+                mCount--;
+
             osg::PositionAttitudeTransform *pat = dynamic_cast<osg::PositionAttitudeTransform *> (node);
+            osg::Quat* rot;
+            rot->makeRotate(osg::DegreesToRadians(2.0), osg::Z_AXIS);
+            osg::Matrix mat1 = osg::Matrix::translate(osg::Vec3(1.f, 1.f, 0.f));
+            //osg::Matrix mat2 = osg::Matrix::se
+
+
+
             //pat->setAttitude(osg::Quat(osg::DegreesToRadians(-25.0), osg::Vec3(0,1,0)));
             traverse(node, nv);
+
+            if(mCount == 50 || mCount == 0)
+                mUp = !mUp;
         }
     }
 
 protected:
     double mTimeStep{1.0/30.0};
-    unsigned int objIndex;
+    bool mUp{true};
+    unsigned int mCount{0};
     bool& progRunning;
 };
